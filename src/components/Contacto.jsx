64db847a927e419844c02d494bdb5e8f27cab7c2
Natalia Córdoba <nataliacorrdoba@gmail.com>
2024-04-13 16:57:35 -0300

    primeros estilos mobile-first

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -3,40 +3,50 @@ import email from '../assets/icons/email.svg'
 
 export const Contacto = () => {
     return (
-        <section id="contacto" className="bg-custom-white p-3">
+        <section id="contacto" className="bg-custom-white py-8 px-4">
             <h2 className="text-custom-brown font-bold text-2xl pb-2 text-center">Contacto</h2>
             <article>
                 <h3 className="text-lg font-semibold text-center">Reserva tu experiencia</h3>
-                <form action="#" className="bg-custom-pink m-4 p-4 flex flex-col gap-1">
+                <form action="#" className="bg-custom-pink m-4 p-4 flex flex-col gap-1 shadow-lg shadow-custom-brown mb-6 rounded-xl">
                     <label htmlFor="nombre">Ingresa tu nombre</label>
                     <input type="text" placeholder="ingresa tu nombre" className="p-2"/>
                     <label htmlFor="email">Ingresa tu correo</label>
                     <input type="email" placeholder="ingresa tu correo" className="p-2"/>
                     <label htmlFor="mensaje">Ingresa tu mensaje</label>
                     <textarea name="mensaje" id="mensaje" cols="30" rows="10" className="p-2 text-gray-400">Deja tu mensaje aquí</textarea>
-                    <button type="submit" className="bg-custom-orange w-min py-2 px-4 rounded-xl border-custom-brown border-2 mt-3 mx-auto">Reservar</button>
+                    <button type="submit" className="bg-custom-orange w-min py-2 px-4 rounded-xl border-custom-brown border-2 mt-3 mx-auto font-semibold">Reservar</button>
                 </form>
             </article>
             <aside>
                 <article>
-                    <h3>Suscríbete a nuestra newsletter</h3>
-                    <p>Recibe todas las semanas un correo con tips para seguir explorando tu creatividad</p>
-                    <form action="#">
-                        <label htmlFor="email">Ingresa tu corre</label>
-                        <input type="email" placeholder="ingresa tu correo" />
-                        <button type="submit">Suscríbete</button>
+                    <h3 className="text-lg font-semibold text-center">Suscríbete a nuestra newsletter</h3>
+                    <p className='px-4'>Recibe todas las semanas un correo con tips para seguir explorando tu creatividad</p>
+                    <form action="#" className='bg-custom-orange m-4 p-4 flex flex-col gap-1 shadow-lg shadow-custom-pink mb-6 rounded-xl'>
+                        <label htmlFor="email">Ingresa tu correo</label>
+                        <input type="email" placeholder="ingresa tu correo" className='p-2'/>
+                        <button type="submit" className='bg-custom-pink w-min py-2 px-4 rounded-xl border-custom-brown border-2 mt-3 mx-auto font-semibold'>Suscríbete</button>
                     </form>
                 </article>
                 <article>
-                    <h3>También nos puedes contactar por</h3>
-                    <div>
-                        <img src={instagram} alt="ig" />
+                    <h3 className="text-lg font-semibold text-center">También nos puedes contactar por</h3>
+                    <ul>
+                    <a 
+                        href="https://www.instagram.com/" 
+                        target='_blank'
+                        className='flex gap-2 pl-5'
+                    >
+                        <img src={instagram} alt="icono ig" />
                         <p>bebiento_arte</p>
-                    </div>
-                    <div>
-                        <img src={email} alt="correo" />
+                    </a>
+                    <a 
+                        href="" 
+                        target='_blank'
+                        className='flex gap-2 pl-5'
+                    >
+                        <img src={email} alt="icono correo" />
                         <p>bebientoarte@example.com</p>
-                    </div>
+                    </a>
+                    </ul>
                 </article>
             </aside>
         </section>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
+import arrowUp from '../assets/icons/arrowUp.svg'
+
 export const Footer = () => {
     return (
-        <h3 className="bg-custom-brown py-3 text-custom-white font-semibold text-center">
-            Hecho con 🖤 por Natalia Córdoba
-        </h3>
+        <footer>
+            <h3 className="bg-custom-brown py-3 text-custom-white font-semibold text-center">
+                Hecho con 🖤 por Natalia Córdoba
+            </h3>
+            <a href="/#inicio" >
+                <img
+                    className='bg-custom-brown rounded-full border-2 border-custom-white p-1 fixed bottom-3 right-3'
+                    src={arrowUp}
+                    alt="icono flecha para ir al inicio"
+                />
+            </a>
+        </footer>
     )
 }
